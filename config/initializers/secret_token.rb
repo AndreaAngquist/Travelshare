ff4c3f0b709c13d19d6e9a6ccfd8b2a706cd4635
Andrea Angquist <andreaangquist@gmail.com>
2012-03-18 13:22:06 -0700

    added new app to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelShare::Application.config.secret_token = 'd95fba725915fc3e6d15eb4849ea111cc693f9ad61708328906b8b11c5bb8d4bbd716e5988bb88398b380aef53114b40e6b2137ca861185b99307e93774cd75b'
